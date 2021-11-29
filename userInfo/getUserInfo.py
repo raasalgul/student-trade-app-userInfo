f22dd81b02e84e58ba89756a911d392fe9b13227
raasalgul <k.sathishrsurya@gmail.com>
2021-11-29 21:38:31 +0000

    updateUserInfo.py

--- userInfo/getUserInfo.py
+++ userInfo/getUserInfo.py
@@ -4,31 +4,33 @@ from userInfo import app
 from flask import request
 from botocore.exceptions import ClientError
 from dotenv import load_dotenv
-# Loading Environment files
+
+''' Loading Environment files '''
 load_dotenv()
 import os
 
-# Configuring AWS dynomo db
+''' Configuring AWS dynamo db '''
 dynamoDbResource = boto3.resource(os.getenv("AWS_DYNAMO"), region_name=os.getenv("AWS_REGION"))
-# Configuring AWS Cognito db
-CogntioClient = boto3.client(os.getenv("AWS_COGNITO"), region_name=os.getenv("AWS_REGION"))
-table_name=os.getenv("DYNAMO_USER_TABLE")
+''' Configuring AWS Cognito db '''
+cognitoClient = boto3.client(os.getenv("AWS_COGNITO"), region_name=os.getenv("AWS_REGION"))
+table_name = os.getenv("DYNAMO_USER_TABLE")
 
-# getUser() This method get the necessary user information from the user dynamo table.
-@app.route('/get-user',methods=['POST'])
+
+''' getUser() This method get the necessary user information from the user dynamo table. '''
+@app.route('/get-user', methods=['POST'])
 def getUser():
-    response=None
+    response = None
     try:
-        # Connect with User Info Dynamodb table
+        ''' Connect with User Info Dynamodb table '''
         table = dynamoDbResource.Table(table_name)
         logging.log("getUser: Connected to table")
-        # The key filed is used to query the table
-        key={"name":request.json['username'],
-            "institution":request.json['institution']
-        }
-        # Get the entire items from the table
-        response=table.get_item(Key=key)
+        ''' The key field is used to query the table '''
+        key = {"email": request.json['email'],
+               "institution": request.json['institution']
+               }
+        ''' Get the entire items from the table '''
+        response = table.get_item(Key=key)
         logging.log("getUser: Got response from table")
     except ClientError as e:
-            logging.error(e)
+        logging.error(e)
     return response
---
+++ userInfo/updateUserInfo.py
@@ -0,0 +1,78 @@
+import boto3
+import logging
+from userInfo import app
+from flask import request
+from botocore.exceptions import ClientError
+from dotenv import load_dotenv
+
+''' Loading Environment files '''
+load_dotenv()
+import os
+
+''' Configuring AWS dynamo db '''
+dynamoDbResource = boto3.resource(os.getenv("AWS_DYNAMO"), region_name=os.getenv("AWS_REGION"))
+''' Configuring AWS Cognito db '''
+cognitoClient = boto3.client(os.getenv("AWS_COGNITO"), region_name=os.getenv("AWS_REGION"))
+table_name = os.getenv("DYNAMO_USER_TABLE")
+
+''' updateUser() Since the signup service will insert the necessary information like Username, Email, Institution into the
+ table, this method will update that record when user is updating their information with other data. '''
+
+
+@app.route('/update-user', methods=['POST'])
+def updateUser():
+    response = None
+    try:
+        ''' Connect with User Info Dynamodb table '''
+        table = dynamoDbResource.Table(table_name)
+        logging.log("updateUser: Connected to table")
+        ''' The key field is used to query the table '''
+        key = {"email": request.json['email'],
+               "institution": request.json['institution']
+               }
+        ''' The updateString is the query string used to update the table dynamically with different column '''
+        updateString = "set "
+
+        expressionAttribute = {}
+        try:
+            response = table.get_item(Key=key)
+            ''' Framing the dynamic query param values and query string '''
+            if 'Item' in response:
+                if "pictureUrl" in request.json:
+                    updateString += "pictureUrl=:p,"
+                    expressionAttribute[':p'] = request.json['pictureUrl']
+                if "course" in request.json:
+                    updateString += "course=:c,"
+                    expressionAttribute[':c'] = request.json['course']
+                if "address" in request.json:
+                    updateString += "address=:a,"
+                    expressionAttribute[':a'] = request.json['address']
+                if "phoneNumber" in request.json:
+                    updateString += "phoneNumber=:n,"
+                    expressionAttribute[':n'] = request.json['phoneNumber']
+                if "emailTemplate" in request.json:
+                    updateString += "emailTemplate=:e,"
+                    expressionAttribute[':e'] = request.json['emailTemplate']
+                if "paymentInfo" in request.json:
+                    updateString += "paymentInfo=:pm,"
+                    expressionAttribute[':pm'] = request.json['paymentInfo']
+
+                try:
+                    ''' Remove the last comma from the query string '''
+                    updateString = updateString[:-1]
+                    logging.log('update String is {}'.format(updateString))
+                    ''' Update the user info table '''
+                    table.update_item(
+                        Key=key,
+                        UpdateExpression=updateString,
+                        ExpressionAttributeValues=expressionAttribute,
+                        ReturnValues="UPDATED_NEW"
+                    )
+                    logging.log("Value updated in the table")
+                except ClientError as e:
+                    logging.error(e)
+        except ClientError as e:
+            logging.error(e)
+    except ClientError as e:
+        logging.error(e)
+    return response
